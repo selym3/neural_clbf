704a00d6c0bed0e978ce5ea02d826cd4ad40dbb7
JLjw8 <jnl77@cornell.edu>
2024-07-11 12:25:28 -0400

    Revise device type check

--- neural_clbf/training/train_point_wind_system.py
+++ neural_clbf/training/train_point_wind_system.py
@@ -94,7 +94,7 @@ def main(args):
         "logs/point_wind_system/", name=f"commit_{current_git_hash}"
     )
     trainer = pl.Trainer.from_argparse_args(
-        args, logger=tb_logger, reload_dataloaders_every_epoch=True, max_epochs=100
+        args, logger=tb_logger, reload_dataloaders_every_epoch=True, max_epochs=2
     )
 
     # Train
