9b0974f623c71933645dc210cad63a7cc932f364
Myles Pasetsky <myles.pasetsky@gmail.com>
2024-07-22 10:37:59 -0400

    Increase control limits

--- neural_clbf/systems/simple_with_wind.py
+++ neural_clbf/systems/simple_with_wind.py
@@ -54,8 +54,8 @@ class SimpleWithWind(ControlAffineSystem):
     @property
     def control_limits(self) :
         upper_limit = torch.ones(self.n_dims)
-        upper_limit[SimpleWithWind.UX] = 1.0
-        upper_limit[SimpleWithWind.UY] = 1.0
+        upper_limit[SimpleWithWind.UX] = 3.0
+        upper_limit[SimpleWithWind.UY] = 3.0
 
         lower_limit = -1.0 * upper_limit
         
